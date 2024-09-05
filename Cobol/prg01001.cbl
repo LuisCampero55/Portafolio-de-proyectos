52a36ddad1858ca77dbc36d02db67a73650fd9b0
Luis Daniel Campero Granados <71288942+LuisCampero55@users.noreply.github.com>
2024-09-05 10:12:44 -0600

    Add files via upload

---
+++ Cobol/prg01001.cbl
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PRG01001.
+      *  Primer Programa de Prueba
+      *  Campero Granados Luis Daniel
+      *  21 ABR 24
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       010-INICIO.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM PRG01001.
